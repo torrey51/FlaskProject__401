dbfe74167055f9a5e2e6e96d027ea2f4968123dd
LoganFishSticks <153231124+LoganFishSticks@users.noreply.github.com>
2024-10-03 13:36:42 -0700

    10.3.2024
    Changed the popover menus to use modals instead and started trying to add values to the database

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
 app= Flask(__name__)
+
+# MySQL database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@localhost/stockwebsite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# Creating a Stock
+class Stock(db.Model):
+    stock_id = db.Column(db.Integer, primary_key=True)
+    company_name = db.Column(db.String(80), nullable=False)
+    ticker_symbol = db.Column(db.String(4), nullable=False)
+    initial_price = db.Column(db.Float, nullable=False)
+    current_price = db.Column(db.Float, nullable=False)
+    volume = db.Column(db.Float, nullable=False)
+
 
 
 #route 1 (page 1) #Home
@@ -11,7 +28,13 @@ def home():
 @app.route('/market')
 def market():   
     contact = request.args.get('market')
-    return render_template('market.html', market=market)
+    db.create_all()
+    # Creating a stock in the stock database
+    # new_stock = Stock(company_name='NVIDIA Corp', ticker_symbol='NVDA', initial_price=108.10, current_price=108.10, volume=262664898)
+    # db.session.add(new_stock)
+    # db.session.commit()
+    stocks = Stock.query.all()
+    return render_template('market.html', stocks=stocks)
 
 #route 3 (page 3) #about
 @app.route('/portfolio')
